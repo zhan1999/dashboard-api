c10dd7c7b6d2d20f6d9fde4a3e18ee30a541eaf0
Zhan <zhan1999@yahoo.com>
2023-12-14 00:58:22 +0100

    Add class-validator & class-transforner

--- src/users/dto/user-register.dto.ts
+++ src/users/dto/user-register.dto.ts
@@ -1,5 +1,12 @@
+import { IsEmail, IsString } from 'class-validator';
+
 export class UserRegisterDto {
+	@IsEmail({}, { message: 'Wrong email' })
 	email: string;
+
+	@IsString({ message: 'Password not specified' })
 	password: string;
+
+	@IsString({ message: 'Name not specified' })
 	name: string;
 }
